54368d15ded69c842eba2ad52177e13cec39988d
Andrew Johnson <ajohnson.uva@gmail.com>
2018-10-01 14:32:19 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,9 @@
 def turn_count(board)
   turn_number = 1
   board.each do |token|
-    if ${token} == "X" || |token
+    if (token == "X" || token == "O")
+      turn_number += 1
+    end
 end
 
 #current_player
